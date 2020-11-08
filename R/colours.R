514af1e843bc9ec0dd0b6bd299466bc48463185f
Eric Koncina <mail@koncina.eu>
2020-11-08 10:58:05 +0100

    add blue, white  and red colour definitions used to plot heatmaps

--- R/colours.R
+++ R/colours.R
@@ -6,3 +6,10 @@ NULL
 colours_cms <- function() {
   c(CMS1 = "#E89B36", CMS2 = "#0473AB", CMS3 = "#CF78A3", CMS4 = "#0D9D76", NOLBL = "black")
 }
+
+#' Blue, white and red colours used to plot heatmaps.
+#'
+#' @export
+colours_bwr <- function() {
+  c(blue = "#377EB8", "white", red = "#E41A1C")
+}
