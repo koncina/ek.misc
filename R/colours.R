59bbdb8119c9f22c328c8c1bcf272aa18adbda63
Eric Koncina <mail@koncina.eu>
2020-11-08 07:35:11 +0100

    provide colours to plot CMS CRC data

---
+++ R/colours.R
@@ -0,0 +1,8 @@
+NULL
+
+#' The colours associated to the the consensus molecular subtypes (CMS) as used in the original publication.
+#'
+#' @export
+colours_cms <- function() {
+  c(CMS1 = "#E89B36", CMS2 = "#0473AB", CMS3 = "#CF78A3", CMS4 = "#0D9D76", NOLBL = "black")
+}
